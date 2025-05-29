f428328699bc23cf9ccc04ce0676621beb673feb
Mausam Pun <sonicmausam@gmail.com>
2025-05-29 17:32:39 +0545

    fix in the product qa config

--- lib/spree_products_qa/configuration.rb
+++ lib/spree_products_qa/configuration.rb
@@ -2,5 +2,14 @@ module SpreeProductsQa
   class Configuration < Spree::Preferences::Configuration
     # allow anonymous questions
     preference :allow_anonymous, :boolean, default: false
+
+    def load_preferences
+      stored_prefs = Spree::Preference.where("key LIKE 'spree_products_qa/config/%'")
+      
+      stored_prefs.each do |pref|
+        preference_name = pref.key.gsub('spree_products_qa/config/', '')
+        self[preference_name] = pref.value if respond_to?("#{preference_name}=")
+      end
+    end
   end
 end
